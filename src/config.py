4bf43e158de57367cde95aebad673d2e04b1bc2a
SillyusBark <sillyusbark@example.com>
2026-06-16 21:33:19 +0000

    Zagon Higgsfield projekta

---
+++ src/config.py
@@ -0,0 +1,32 @@
+# RULES FOR CONFIG FILE:
+# 1. If you want something from env file, put it there, and just call the os.getenv, we will load it for you.
+# 2. Do not put any imports here. None but 'os'.
+# 3. Only simple definitions. 
+# 4. Always under $folder_of_your_project/src/config.py.
+
+
+import os
+
+NAME = "moj_ai_projekt"
+
+# Ker želiš vse poganjati na tem istem strežniku, vpiši lokalni IP naslov (localhost)
+HOSTS = [
+    "127.0.0.1",
+]
+
+# Tvoj uporabnik na strežniku je 'ubuntu'
+HOSTS_USER = "ubuntu"
+
+# Standardni port za SSH je 22
+HOSTS_PORT = 22
+
+# Nastavi število procesov (GPU grafičnih kartic). Če nimaš GPU-ja, pusti 1 ali 0.
+NUM_PROCESSES = 1
+
+NAME = "moj_ai_projekt"
+
+HOSTS = ["1.2.3.4"]
+HOSTS_USER = "ubuntu"
+HOSTS_PORT = 22
+
+NUMBER_OF_PROCESSES_PER_NODE = 2
